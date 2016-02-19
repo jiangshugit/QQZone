b24b862b65668f30d7280314cec035a1dd3f84af
将书 <791495799@qq.com>
2016-02-19 14:26:15 +0800

    初始化项目

---
+++ QQZone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQZone
+//
+//  Created by 依山观澜 on 16/2/19.
+//  Copyright © 2016年 依山观澜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQZone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQZone
+//
+//  Created by 依山观澜 on 16/2/19.
+//  Copyright © 2016年 依山观澜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
